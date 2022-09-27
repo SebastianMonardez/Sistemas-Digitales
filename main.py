ac381661f2f630932d24b81751b51a5b08eb6c67
Sebastian Monardez <sebastian.monardez.20@alumnos.uda.cl>
2022-09-27 19:44:48 -0300

    Agrega archivo main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from codificar_hamming import *
+from decodificar_hamming import *
+
+palabra = [1,1,0,0,0,0,0,1]
+res_esperado = [0,1,1,0,1,0,0,1,0,0,0,1]
+
+p_codificada = codificar_hamming(palabra)
+
+if(p_codificada == res_esperado):
+    print("Codificacion Correcta")
+else:
+    print("Codificacion Incorrecto")
+
+p_decodificada = decodificar_hamming(p_codificada)
+
+if(p_decodificada == palabra):
+    print("Decodificacion Correcta")
+else:
+    print("Decodificacion Incorrecta")
