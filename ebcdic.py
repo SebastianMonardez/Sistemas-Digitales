2909bdef8eb740df0c33b22fb4e0b772813cee6a
IgnacioZenteno <ignacio.zenteno.20@alumnos.uda.cl>
2022-10-05 02:02:33 -0300

    Se arreglo un bug

--- ebcdic.py
+++ ebcdic.py
@@ -1,7 +1,17 @@
-with open("ebcdic.txt") as citas:
+with open("ebcdic.txt", 'r') as datos:
     matriz = []
-    for linea in citas:
-        campos = linea.strip()[1:-1].replace('','').split(",")
-        matriz.append(campos)
+    for linea in datos:
+        matriz.append(linea.strip().split(","))
 
-print(matriz)
+def encontrar_bits(caracter):
+    for i in matriz:
+        if(i[0] == caracter):
+            return [int(i) for i in i[1]]
+
+def encontrar_caracter(bits):
+    aux=""
+    for i in bits:
+        aux+=str(i)
+    for i in matriz:
+        if(i[1] == aux):
+            return i[0]
