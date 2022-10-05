499bf0689e9f8fafa7e2716e0ec8bfa68a5add23
Sebastian Monardez <sebastian.monardez.20@alumnos.uda.cl>
2022-10-05 01:16:58 -0300

    Agrega ebcdic.py

---
+++ ebcdic.py
@@ -0,0 +1,14 @@
+with open("ebcdic.txt", 'r') as datos:
+    matriz = []
+    for linea in datos:
+        matriz.append(linea.strip().split(","))
+
+def encontrar_bits(caracter):
+    for i in matriz:
+        if(i[0] == caracter):
+            return i[1]
+
+def encontrar_caracter(bits):
+    for i in matriz:
+        if(i[1] == bits):
+            return i[0]
